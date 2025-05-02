ccadfb23c8acbea6a4990db96cf964fa779027e2
vizz1234 <vishdd4@gmail.com>
2025-05-02 18:03:12 +0530

    2 May 2025 1802

---
+++ longest_common_subsequence.py
@@ -0,0 +1,25 @@
+def lcs(S1, S2):
+    m = len(S1)
+    n = len(S2)
+
+    # Initializing a matrix of size (m+1)*(n+1)
+    dp = [[0] * (n + 1) for x in range(m + 1)]
+
+    # Building dp[m+1][n+1] in bottom-up fashion
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if S1[i - 1] == S2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j],
+                               dp[i][j - 1])
+
+    # dp[m][n] contains length of LCS for S1[0..m-1]
+    # and S2[0..n-1]
+    return dp[m][n]
+
+
+if __name__ == "__main__":
+    S1 = "AGGTAB"
+    S2 = "GXTXAYB"
+    print(lcs(S1, S2))
