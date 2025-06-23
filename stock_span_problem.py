339062f5d810f844695c247a44fc491c71b17522
vizz1234 <vishdd4@gmail.com>
2025-06-23 13:40:09 +0530

    23 June 2025 1340

--- stock_span_problem.py
+++ stock_span_problem.py
@@ -1,38 +1,22 @@
-# Python program to calculate stock span
-# values using a stack
-
-# Function to calculate stock span values
-def calculateSpan(arr):
-
-    n = len(arr)
-    span = [0] * n  
-    stk = []
-
-    # Process each day's price
-    for i in range(n):
-
-        # Remove elements from the stack while the current
-        # price is greater than or equal to stack's top price
-        while stk and arr[stk[-1]] <= arr[i]:
-            stk.pop()
-
-        # If stack is empty, all elements to the left are smaller
-        # Else, top of the stack is the last greater element's index
-        if not stk:
-            span[i] = (i + 1)
-        else:
-            span[i] = (i - stk[-1])
-
-        # Push the current index to the stack
-        stk.append(i)
-
-    return span
-
-
-if __name__ == "__main__":
-
-    arr = [10, 4, 5, 90, 120, 80]
-    span = calculateSpan(arr)
+class Solution:
+    def calculateSpan(self, arr):
+        #write code here
+        stack = []
+        output = []
+        for i in range(len(arr)):
+            val = arr[i]
+            while stack and arr[stack[-1]] <= val:
+                stack.pop()
+            
+            if not stack:
+                span = i + 1
+            else:
+                span = i - stack[-1]
     
-    for x in span:
-        print(x, end=" ")
+            output.append(span)
+            stack.append(i)
+            
+        return output
+
+s = Solution()
+print(s.calculateSpan([100, 80, 60, 70, 60, 75, 85]))
