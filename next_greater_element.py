a61324672ac55ca25a2d4b0a0b4888702193ac06
vizz1234 <vishdd4@gmail.com>
2025-06-23 13:14:42 +0530

    23 June 2025 1314

--- next_greater_element.py
+++ next_greater_element.py
@@ -1,30 +1,19 @@
-# Python implementation to find the next
-# greater element using a stack
-def nextLargerElement(arr):
-    n = len(arr)
-    res = [-1] * n  
-    stk = []  
+class Solution:
+    def nextLargerElement(self, arr):
+        # code here
+        n = len(arr)
+        output = [-1] * len(arr)
+        stack = []
+        for i in range(n - 1, -1, -1):
+            val = arr[i]
+            while stack and stack[-1] <= val:
+                stack.pop()
+            if stack:
+                output[i] = stack[-1]
+            stack.append(val)
+        return output
 
-    # Traverse the array from right to left
-    for i in range(n - 1, -1, -1):
-
-        # Pop elements from the stack that are less 
-        # than or equal to the current element
-        while stk and arr[stk[-1]] <= arr[i]:
-            stk.pop()
-
-        # If the stack is not empty, the element at the 
-        # top of the stack is the next greater element
-        if stk:
-            res[i] = arr[stk[-1]]
-
-        # Push the current index onto the stack
-        stk.append(i)
-
-    return res
 
 if __name__ == "__main__":
-  
-    arr = [6, 8, 0, 1, 3]
-    res = nextLargerElement(arr)
-    print(" ".join(map(str, res)))
+    sol = Solution()
+    print(sol.nextLargerElement([1, 3, 2, 4]))
