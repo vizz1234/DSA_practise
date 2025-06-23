632d63097a801d6c42ff0d82984dc5e0d35799f0
vizz1234 <vishdd4@gmail.com>
2025-06-23 11:34:36 +0530

    23 June 2025 1134

--- longest_valid_parenthesis.py
+++ longest_valid_parenthesis.py
@@ -1,39 +1,24 @@
-# Python program to find length of the
-# longest valid substring
-
-def maxLength(s):
-    stack = []
-
-    # Push -1 as the initial index to 
-    # handle the edge case
-    stack.append(-1)
-    maxLen = 0
-
-    # Traverse the string
-    for i in range(len(s)):
-
-        # If we encounter an opening parenthesis,
-        # push its index
-        if s[i] == '(':
-            stack.append(i)
-        else:
-
-            # If we encounter a closing parenthesis, 
-            # pop the stack
-            stack.pop()
-
-            # If stack is empty, push the current index
-            # as a base for the next valid substring
-            if not stack:
+class Solution:
+    def maxLength(self, s):
+        # code here
+        stack = [-1]
+        n = len(s)
+        longPar = 0
+        
+        for i in range(n):
+            
+            if s[i] == '(':
                 stack.append(i)
             else:
-              
-                # Update maxLength with the current length 
-                # of the valid parentheses substring
-                maxLen = max(maxLen, i - stack[-1])
+                stack.pop()
+                
+                if not stack:
+                    stack.append(i)
+                
+                else:
+                    longPar = max(longPar, i - stack[-1])
+        
+        return longPar
 
-    return maxLen
-
-if __name__ == "__main__":
-    s = ")()())"
-    print(maxLength(s))
+s = Solution()
+print(s.maxLength("(()))"))
