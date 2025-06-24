95f2934abdda56a254c9976d64180ea62bda1f33
vizz1234 <vishdd4@gmail.com>
2025-06-24 16:46:20 +0530

    24 June 2025 1645

--- histogram_max_area.py
+++ histogram_max_area.py
@@ -1,59 +1,27 @@
-# Python program to find the largest rectangular area possible 
-# in a given histogram 
+class Solution:
+    def getMaxArea(self,arr):
+        #code here
+        stack = []
+        n = len(arr)
+        maxArea = 0
+        
+        for i in range(n):
+            while stack and arr[i] < arr[stack[-1]]:
+                top = stack.pop()
+                width = i if not stack else i - stack[-1] - 1
+                area = arr[top] * width
+                maxArea = max(area, maxArea)
+            stack.append(i)
+        
+        i = n
+        
+        while stack:
+            top = stack.pop()
+            width = i if not stack else i - stack[-1] - 1
+            area = arr[top] * width
+            maxArea = max(area, maxArea)
+        
+        return maxArea
 
-# Function to find next smaller for every element
-def nextSmaller(arr):
-    n = len(arr)
-    
-    # Initialize with n for the cases when next smaller
-    # does not exist
-    nextS = [n] * n
-    st = []
-    
-    # Traverse all array elements from left to right
-    for i in range(n):
-        while st and arr[i] < arr[st[-1]]:
-            # Setting the index of the next smaller element
-            # for the top of the stack
-            nextS[st.pop()] = i
-        st.append(i)
-    
-    return nextS
-
-# Function to find previous smaller for every element
-def prevSmaller(arr):
-    n = len(arr)
-    
-    # Initialize with -1 for the cases when prev smaller
-    # does not exist
-    prevS = [-1] * n
-    st = []
-    
-    # Traverse all array elements from left to right
-    for i in range(n):
-        while st and arr[i] < arr[st[-1]]:
-            st.pop()
-        if st:
-            prevS[i] = st[-1]
-        st.append(i)
-    
-    return prevS
-
-# Function to calculate the maximum rectangular
-# area in the Histogram
-def getMaxArea(arr):
-    prevS = prevSmaller(arr)
-    nextS = nextSmaller(arr)
-    maxArea = 0
-    
-    # Calculate the area for each arrogram bar
-    for i in range(len(arr)):
-        width = nextS[i] - prevS[i] - 1
-        area = arr[i] * width
-        maxArea = max(maxArea, area)
-    
-    return maxArea
-
-if __name__ == "__main__":
-    arr = [60, 20, 50, 40, 10, 50, 60]
-    print(getMaxArea(arr))
+s = Solution()
+print(s.getMaxArea([6, 2, 5, 4, 5, 1, 6]))
