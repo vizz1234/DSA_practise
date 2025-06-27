735df679d9a88c35b091aeb2d5f5d026479b2bf5
vizz1234 <vishdd4@gmail.com>
2025-06-27 11:25:37 +0530

    27 June 2025 1125

--- decode_string.py
+++ decode_string.py
@@ -1,57 +1,27 @@
-# Python program to decode a string recursively
-# encoded as count followed substring
+class Solution:
+    def decodedString(self, s):
+        # code here
+        curS = ''
+        curNum = 0
+        stack = []
+        
+        for char in s:
+            if char.isdigit():
+                curNum = 10 * curNum + int(char)
+            
+            elif char == '[':
+                stack.append((curNum, curS))
+                curNum = 0
+                curS = ''
+            
+            elif char == ']':
+                mulNum, mulStr = stack.pop()
+                curS = mulStr + curS * mulNum
+                
+            else:
+                curS += char
+        
+        return curS
 
-# Function to decode the pattern string 's'
-def decodedString(s):
-    numStack = []
-    charStack = []
-    temp = ""
-    res = ""
-
-    i = 0
-    while i < len(s):
-        cnt = 0
-
-        # If Digit, convert it into number and
-        # push it into integer stack.
-        if s[i].isdigit():
-            while i < len(s) and s[i].isdigit():
-                cnt = cnt * 10 + int(s[i])
-                i += 1
-            i -= 1
-            numStack.append(cnt)
-
-        # If closing bracket ']' is encountered
-        elif s[i] == ']':
-            temp = ""
-
-            cnt = numStack.pop()
-
-            # Pop element till opening bracket '[' is not found
-            # in the character stack.
-            while charStack[-1] != '[':
-                temp = charStack.pop() + temp
-            charStack.pop()
-
-            # Repeating the popped string 'temp' count number of times.
-            res = temp * cnt
-
-            # Push it in the character stack.
-            for c in res:
-                charStack.append(c)
-
-            res = ""
-        else:
-            charStack.append(s[i])
-        i += 1
-
-    # Pop all the elements, make a string and return.
-    while charStack:
-        res = charStack.pop() + res
-
-    return res
-
-
-if __name__ == "__main__":
-    s = "3[b2[ca]]"
-    print(decodedString(s))
+s = Solution()
+print(s.decodedString("3[a2[c]]"))
