83e483b1c74f6e1a96b0f5ea373420cbb8c3f6fd
vizz1234 <vishdd4@gmail.com>
2025-06-29 08:35:54 +0530

    29 June 2025 0835

--- longest_bounded_diff_subarray.py
+++ longest_bounded_diff_subarray.py
@@ -1,65 +1,36 @@
-# Python program to find the longest subarray where the
-# absolute difference between any two elements is not
-# greater than X
-
 from collections import deque
-
-# Functio+1n that prints the longest sub-array
-# where the absolute difference between any
-# two element is not greater than X
-def longestSubarray(arr, x):
-    
-    # Monotonic Queue to store maximum and minimum elements
-    minQueue = deque()
-    maxQueue = deque()
-    
-    # Pointers to mark the range of current subarray
-    n = len(arr)
-    start = end = 0
-    
-    # Pointers to mark the range of maximum subarray
-    resStart = resEnd = 0
-    while end < n:
+class Solution:
+    def longestSubarray(self, arr, x):
+        #code here 
+        left = 0
+        incDq = deque()
+        decDq = deque()
+        maxWin = 0
+        start = 0
+        n = len(arr)
         
-        # Pop the elements greater than current element
-        # from min Queue
-        while minQueue and arr[minQueue[-1]] > arr[end]:
-            minQueue.pop()
+        for right in range(n):
             
-        # Pop the elements smaller than current element
-        # from max Queue
-        while maxQueue and arr[maxQueue[-1]] < arr[end]:
-            maxQueue.pop()
+            while incDq and arr[right] < arr[incDq[-1]]:
+                incDq.pop()
+            incDq.append(right)
             
-        # Push the current index to both the queues
-        minQueue.append(end)
-        maxQueue.append(end)
-        
-        # Check if the subarray has maximum difference less
-        # than X
-        while arr[maxQueue[0]] - arr[minQueue[0]] > x:
-                   
-            # Reduce the length of sliding window by moving
-            # the start pointer
-            if start == minQueue[0]:
-                minQueue.popleft()
-            if start == maxQueue[0]:
-                maxQueue.popleft()
-            start += 1
-        
-        # Maximize the subarray length
-        if end - start > resEnd - resStart:
-            resStart, resEnd = start, end
-
-        end += 1
-
-    # Return the longest sub-array
-    return arr[resStart:resEnd+1]
-
-if __name__ == "__main__":
-    arr = [8, 4, 2, 6, 7]
-    x = 4
-
-    res = longestSubarray(arr, x)
+            while decDq and arr[right] > arr[decDq[-1]]:
+                decDq.pop()
+            decDq.append(right)
+            
+            while arr[decDq[0]] - arr[incDq[0]] > x:
+                left += 1
+                if decDq[0] < left:
+                    decDq.popleft()
+                if incDq[0] < left:
+                    incDq.popleft()
+            
+            if right - left + 1 > maxWin:
+                maxWin = right - left + 1
+                start = left
+            
+        return arr[start : start + maxWin]
     
-    print(*res)
+sol = Solution()
+print(sol.longestSubarray([8, 4, 2, 6, 7], 4))
