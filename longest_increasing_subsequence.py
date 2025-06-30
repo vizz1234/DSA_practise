991e4eab72e355eb85169c4a86c6b9e5630b9d13
vizz1234 <vishdd4@gmail.com>
2025-06-30 13:16:47 +0530

    30 June 2025 1316

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -1,35 +1,16 @@
-def lisEndingAtIdx(arr, idx, memo):
-  
-    # Base case
-    if idx == 0:
-        return 1
-
-    # Check if the result is already computed
-    if memo[idx] != -1:
-        return memo[idx]
-
-    # Consider all elements on left of i,
-    # recursively compute LISs ending with 
-    # them and consider the largest
-    mx = 1
-    for prev in range(idx):
-        if arr[prev] < arr[idx]:
-            mx = max(mx, lisEndingAtIdx(arr, prev, memo) + 1)
-
-    # Store the result in the memo array
-    memo[idx] = mx
-    return memo[idx]
-
-def lis(arr):
-    n = len(arr)
-
-    memo = [-1] * n
-  
-    res = 1
-    for idx in range(1, n):
-        res = max(res, lisEndingAtIdx(arr, idx, memo))
-    return res
-
-if __name__ == "__main__":
-    arr = [10, 22, 9, 33, 21, 50, 41, 60]
-    print(lis(arr))
+class Solution:
+    def lis(self, arr):
+        # code here
+        if not arr:
+            return 0
+        n = len(arr)
+        dp = [1] * n
+        
+        for i in range(n):
+            for j in range(i):
+                if arr[j] < arr[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        return max(dp)
+sol = Solution()
+print(sol.lis([1, 2, 3, 4, 5]))
