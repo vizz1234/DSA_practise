b42aa3030afdd9d4da62a146c8134cfdf3d974fc
vizz1234 <vishdd4@gmail.com>
2025-07-03 11:45:23 +0530

    03 July 2025 1145

--- longest_palindrome_in_a_string.py
+++ longest_palindrome_in_a_string.py
@@ -1,39 +1,34 @@
-# Python program to find the longest
-# palindromic substring.
-
-# Function to find the longest palindrome substring
-def longestPalindrome(s):
-    n = len(s)
-    dp = [[False] * n for _ in range(n)]
-    
-    start, maxLen = 0, 1
-
-    # All substrings of length 1 are palindromes
-    for i in range(n):
-        dp[i][i] = True
-
-    # Check for sub-string of length 2
-    for i in range(n - 1):
-        if s[i] == s[i + 1]:
-            dp[i][i + 1] = True
-            if maxLen < 2:
-                start = i
-                maxLen = 2
-
-    # Check for lengths greater than 2
-    for k in range(3, n + 1):
-        for i in range(n - k + 1):
-            j = i + k - 1
-
-            if dp[i + 1][j - 1] and s[i] == s[j]:
-                dp[i][j] = True
-
-                if k > maxLen:
+class Solution:
+    def longestPalindrome(self, s):
+        # code here
+        n = len(s)
+        dp = [[False]*n for _ in range(n)]
+        
+        start = 0
+        maxLen = 1
+        curLen = 1
+        
+        for i in range(n - 1):
+            dp[i][i] = True
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                curLen = 2
+                if curLen > maxLen:
                     start = i
-                    maxLen = k
+                    maxLen = curLen
+        dp[n - 1][n - 1] = True
+        
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    curLen = length
+                    if curLen > maxLen:
+                        start = i
+                        maxLen = curLen
+        
+        return s[start : start + maxLen]
 
-    return s[start:start + maxLen]
-
-if __name__ == "__main__":
-    s = "aaaabbaa"
-    print(longestPalindrome(s))
+sol = Solution()
+print(sol.longestPalindrome("babad"))
