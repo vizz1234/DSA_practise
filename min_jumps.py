77cf6162e57ba8bd080fc9f38b42b7e9d8a71fa3
vizz1234 <vishdd4@gmail.com>
2025-07-05 16:49:30 +0530

    05 July 2025 1649

--- min_jumps.py
+++ min_jumps.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(1, n):
             for j in range(i):
                 if j + arr[j] >= i:
-                    dp[i] = min(dp[i], dp[j] + 1)                
+                    dp[i] = min(dp[i], dp[j] + 1)
         return dp[-1] if dp[-1] != float('inf') else -1
 
 sol = Solution()
