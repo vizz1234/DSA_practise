e17669990f6c4eba51d6a23b052e247041ac0583
vizz1234 <vishdd4@gmail.com>
2025-07-05 11:41:24 +0530

    05 July 2025 1141

---
+++ min_jumps.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minJumps(self, arr):
+        # code here
+        n = len(arr)
+        dp = [float('inf')] * n
+        dp[0] = 0
+        for i in range(1, n):
+            for j in range(i):
+                if j + arr[j] >= i:
+                    dp[i] = min(dp[i], dp[j] + 1)                
+        return dp[-1] if dp[-1] != float('inf') else -1
+
+sol = Solution()
+print(sol.minJumps([2, 3, 1, 1, 4]))
