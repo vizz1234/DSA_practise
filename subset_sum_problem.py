b3bc3d22805e9bb36b142af11a38ed4934e038f4
vizz1234 <vishdd4@gmail.com>
2025-07-07 14:16:26 +0530

    07 July 2025 1416

--- subset_sum_problem.py
+++ subset_sum_problem.py
@@ -1,21 +1,39 @@
-def is_subset_sum(arr, total_sum):
-    n = len(arr)
-    dp = [[False] * (total_sum + 1) for _ in range(n + 1)]
+# def is_subset_sum(arr, total_sum):
+#     n = len(arr)
+#     dp = [[False] * (total_sum + 1) for _ in range(n + 1)]
 
-    # If sum is 0, then answer is True (empty subset)
-    for i in range(n + 1):
-        dp[i][0] = True
+#     # If sum is 0, then answer is True (empty subset)
+#     for i in range(n + 1):
+#         dp[i][0] = True
 
-    # Fill the subset table in bottom-up manner
-    for i in range(1, n + 1):
-        for j in range(1, total_sum + 1):
-            if arr[i - 1] > j:
-                dp[i][j] = dp[i - 1][j]  # can't include arr[i-1]
-            else:
-                dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
+#     # Fill the subset table in bottom-up manner
+#     for i in range(1, n + 1):
+#         for j in range(1, total_sum + 1):
+#             if arr[i - 1] > j:
+#                 dp[i][j] = dp[i - 1][j]  # can't include arr[i-1]
+#             else:
+#                 dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
 
-    return dp[n][total_sum]
+#     return dp[n][total_sum]
 
-arr = [3, 34, 4, 12, 5, 2]
-total_sum = 9
-print(is_subset_sum(arr, total_sum))
+# arr = [3, 34, 4, 12, 5, 2]
+# total_sum = 9
+# print(is_subset_sum(arr, total_sum))
+
+# With O(sum) space 
+class Solution:
+    def isSubsetSum (self, arr, sum):
+        # code here 
+        n = len(arr)
+        dp = [False] * (sum + 1)
+        dp[0] = True
+        
+        for num in arr:
+            for i in range(sum, num - 1, -1):
+                if dp[i - num]:
+                    dp[i] = True
+        
+        return dp[sum]
+
+sol = Solution()
+print(sol.isSubsetSum([3, 34, 4, 12, 5, 2], 9))
