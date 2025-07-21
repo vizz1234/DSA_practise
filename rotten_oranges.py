40d965cf41ad1299c836150f158d4c5e5486c2a3
vizz1234 <vishdd4@gmail.com>
2025-07-21 15:58:15 +0530

    21 July 2025 1558

---
+++ rotten_oranges.py
@@ -0,0 +1,32 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, mat):
+        #Code here
+        n = len(mat)
+        m = len(mat[0])
+        q = deque()
+        fresh = 0
+        
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 2:
+                    q.append((i, j, 0))
+                elif mat[i][j] == 1:
+                    fresh += 1
+        
+        time = 0
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
+        while q:
+            
+            i, j, t = q.popleft()
+            time = max(time, t)
+            
+            for dx, dy in directions:
+                di, dj = dx + i, dy + j
+                if 0 <= di < n and 0 <= dj < m and mat[di][dj] == 1:
+                    mat[di][dj] = 2
+                    fresh -= 1
+                    q.append((di, dj, t + 1))
+        
+        return time if fresh == 0 else -1
