b96f3ab483a6886085380b228f182e46b9cfcdb7
vizz1234 <vishdd4@gmail.com>
2026-02-27 20:25:46 +0530

    27 Feb 2026 2025

---
+++ subarrays_with_atmost_k_distinct_integers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countAtMostK(self, arr, k):
+        # Code here
+        n = len(arr)
+        freq = {}
+        count = 0
+        left = 0
+        for right in range(n):
+            freq[arr[right]] = freq.get(arr[right], 0) + 1
+            while len(freq) > k:
+                freq[arr[left]] -= 1
+                if freq[arr[left]] == 0:
+                    del freq[arr[left]]
+                left += 1
+            count += right - left + 1
+        return count
+sol = Solution()
+arr = [1, 2, 3, 4, 5]
+k = 3
+print(sol.countAtMostK(arr, k))
