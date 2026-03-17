6b8b7a3dd8c9c7469d439b286a2fc8bbef945dca
vizz1234 <vishdd4@gmail.com>
2026-03-17 20:07:50 +0530

    17 Mar 2026 2007

---
+++ number_of_graph_cuts_x_axis.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+class Solution:
+    def touchedXaxis(self, arr):
+        # code here
+        if len(arr) == 0:
+            return 0
+        count = 0
+        s = arr[0]
+        n = len(arr)
+        for i in range(1, n):
+            cur_s = s + arr[i]
+            if (s < 0 and cur_s >= 0) or (s > 0 and cur_s <= 0):
+                count += 1
+            s = cur_s
+        return count
+arr = [2, 5, -9, 4]
+obj = Solution()
+print(obj.touchedXaxis(arr))
