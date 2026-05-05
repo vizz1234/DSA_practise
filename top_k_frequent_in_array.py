4951d17315ddd5eacd8da621cd1266ea11c83bbe
vizz1234 <vishdd4@gmail.com>
2026-05-05 19:56:16 +0530

    05 May 2026 1956 Tree problem

---
+++ top_k_frequent_in_array.py
@@ -0,0 +1,22 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFreq(self, arr, k):
+        # Code here
+        freq = Counter(arr)
+        heap = []
+        
+        for num, f in freq.items():
+            heapq.heappush(heap, (-f, -num))
+        
+        result = []
+        
+        for _ in range(k):
+            f, num = heapq.heappop(heap)
+            result.append(-num)
+        
+        return result
+        
+sol = Solution()
+print(sol.topKFreq([1,1,1,2,2,3], 2))      
