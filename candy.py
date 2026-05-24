b8d8d149ea4a05133a2378ab2e4999ca87b61b15
vizz1234 <vishdd4@gmail.com>
2026-05-24 22:06:34 +0530

    24 May 2026 2206 Greedy problem

---
+++ candy.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCandy(self, arr):
+        # Code here
+        n = len(arr)
+        candies = [1] * n
+        
+        for i in range(1, n):
+            if arr[i] > arr[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        for i in range(n - 2, -1, -1):
+            if arr[i] > arr[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        
+        return sum(candies)
+
+sol = Solution()
+print(sol.minCandy([1, 3, 2, 1]))
