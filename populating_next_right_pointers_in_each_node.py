14cbcf5d546b9fc6e480210a400d199ec21b6dc4
vizz1234 <vishdd4@gmail.com>
2026-06-15 15:36:51 +0530

    15 June 2026 1536 Binary Tree Problem

---
+++ populating_next_right_pointers_in_each_node.py
@@ -0,0 +1,38 @@
+
+from typing import Optional
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+
+        leftmost = root
+
+        while leftmost and leftmost.left:
+            head = leftmost
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                head = head.next
+            leftmost = leftmost.left
+        
+        return root
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+sol = Solution()
+print(sol.connect(root))  
