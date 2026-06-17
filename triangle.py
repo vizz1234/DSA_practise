5df4d4747e008a56dc4f70907fb2a0c4cd16c0bf
vizz1234 <vishdd4@gmail.com>
2026-06-17 15:20:22 +0530

    17 June 2026 1520 DP Problem

---
+++ triangle.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1][:]
+        n = len(triangle)
+
+        for i in range(n - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+        
+        return dp[0]
+
+sol = Solution()
+print(sol.minimumTotal([[2],[3,4],[6,5,7],[4,1,-1,3]]))
