6e5a25f0920d1a101f121bbf1f800c11b30ed0f3
vizz1234 <vishdd4@gmail.com>
2026-07-06 14:15:40 +0530

    6 July 2026 1415 Linked List Problem

---
+++ linkedlist_cycle_2.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        flag = 0
+        
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                flag = 1
+                break
+        
+        slow = head
+        
+        while slow and fast and slow != fast:
+            slow = slow.next
+            fast = fast.next
+        
+        return slow if flag else None
+
+sol = Solution()
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = head
+print(sol.detectCycle(head).val)
