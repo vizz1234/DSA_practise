5a43db208001a6f97c18c45a46dbc7b2b56a942e
vizz1234 <vishdd4@gmail.com>
2026-08-03 19:51:20 +0530

    3 Aug 2026 1951 Graph Problem

---
+++ number_of_islands.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] != '1':
+                return
+            
+            grid[r][c] = '0'
+
+            dfs(r, c + 1)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r - 1, c)
+        
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        
+        return count
+
+sol = Solution()
+print(sol.numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
+            
