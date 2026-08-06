9b9c7b65dc435f85744c96d7fab148e6c87a901a
vizz1234 <vishdd4@gmail.com>
2026-08-06 12:43:57 +0530

    6 Aug 2026 1243 Linked List Problem

---
+++ remove_linked_list_elements.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
+        dummy = ListNode(0, head)
+        curr = dummy
+
+        while curr.next:
+
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return dummy.next
+    
+sol = Solution()
+print(sol.removeElements(head = [1,2,6,3,4,5,6], val = 6))
+        
