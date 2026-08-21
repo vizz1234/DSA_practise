07cc9b5a7f914da153fb148c3e226401e9fdce0f
vizz1234 <vishdd4@gmail.com>
2026-08-21 19:28:43 +0530

    21 Aug 2026 1928 DP Problem

---
+++ maximal_square.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+
+        r, c = len(matrix), len(matrix[0])
+        dp = [[0] * (c + 1) for _ in range(r + 1)]
+        max_count = 0
+
+        for i in range(1, r + 1):
+            for j in range(1, c + 1):
+
+                if matrix[i - 1][j - 1] == "1":
+
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j])
+                    max_count = max(max_count, dp[i][j])
+        
+        return max_count ** 2
+
+sol = Solution()
+print(sol.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
