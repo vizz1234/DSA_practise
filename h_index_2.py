60f45e3824a45f8f10035de3bcc7e69fa4e9cbbc
vizz1234 <vishdd4@gmail.com>
2026-09-07 13:26:53 +0530

    7 Sep 2026 1326 Binary Search

---
+++ h_index_2.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+        left, right = 0, len(citations) - 1
+        n = len(citations)
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if citations[mid] >= (n - mid):
+                right = mid - 1
+            
+            else:
+                left = mid + 1
+                
+        
+        return n - left
+
+sol = Solution()
+print(sol.hIndex([0, 1, 3, 5, 6]))
